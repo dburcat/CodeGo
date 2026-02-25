e8df8d35f0f86ce4bbaaee68077fc9c6de482dbc
david <dburcad@gmail.com>
2026-02-25 10:19:07 -0500

    working on python

---
+++ python/hello_world.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+x = 5
+print(x)
+print(x + 5)
+print(x)
+x = x + 10
+print(x)
+
+def println(s):
+    print(s)
+
+println("your name here!")
---
+++ python/hello_zip.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+x = 5
+print(x)
+print(x+5)
+print(x+10)
+def println(s):
+	print(s)
+println("Your name here!")
